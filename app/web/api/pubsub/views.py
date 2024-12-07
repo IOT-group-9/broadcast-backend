0cfa31ae600942d5113ddbfce9376b0e93fe6dde
qazer25 <1006005@mymail.sutd.edu.sg>
2024-12-07 14:35:40 +0800

    added replicias and updated the publish endpoint

--- app/web/api/pubsub/views.py
+++ app/web/api/pubsub/views.py
@@ -30,7 +30,7 @@ async def publish_parking(
 ) -> dict:
     """Send updated details to subscribed apps."""
     map = await Map.select().where(Map.parking_place == parking_place_id, Map.level_no == level).first()
-    map_slots = await MapSlot.select().where(MapSlot.map.id == map)
+    map_slots = await MapSlot.select().where(MapSlot.map == map.get('id'))
     await pubsub.publish(
         topics=[f"{parking_place_id}/{level}"],
         data={"map": map, "map_slots": map_slots},
